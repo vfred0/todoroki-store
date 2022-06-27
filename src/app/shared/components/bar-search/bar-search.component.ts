042f2037a2384d8e9bc6fd2c7b8bf69f1da377e3
MARCOS TERAN <marcos-teran-unemi@guayastec.edu.ec>
2022-06-26 19:09:54 -0500

    component bar-search

---
+++ src/app/shared/components/bar-search/bar-search.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-search',
+  templateUrl: './bar-search.component.html'
+})
+export class BarSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
