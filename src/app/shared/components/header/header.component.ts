a735adaa8aae38804022cc4f5c8ac1cc9ae09de1
MARCOS TERAN <marcos-teran-unemi@guayastec.edu.ec>
2022-06-29 11:00:43 -0500

    review

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,17 +6,35 @@ import { TypeButton } from '@core/models/TypeButton';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  activeModal: string = '';
+  activeModalToggle: string = '';
+  activeModalSearch: string = '';
+  activeModalShop: string = '';
   typeButton: TypeButton = TypeButton.Default;
 
   constructor() {}
 
   onToggle(event: any): void {
     event.preventDefault();
-    if (this.activeModal === 'is-active') {
-      this.activeModal = '';
+    if (this.activeModalToggle === 'is-active') {
+      this.activeModalToggle = '';
     } else {
-      this.activeModal = 'is-active';
+      this.activeModalToggle = 'is-active';
+    }
+  }
+  onSearch(event: any): void {
+    event.preventDefault();
+    if (this.activeModalSearch === 'is-active') {
+      this.activeModalSearch = '';
+    } else {
+      this.activeModalSearch = 'is-active';
+    }
+  }
+  onShop(event: any): void {
+    event.preventDefault();
+    if (this.activeModalShop === 'is-active') {
+      this.activeModalShop = '';
+    } else {
+      this.activeModalShop = 'is-active';
     }
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { CardComponent } from './components/card/card.component';
 import { TagComponent } from './components/tag/tag.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { BarSearchComponent } from './components/bar-search/bar-search.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ButtonComponent, CardComponent, TagComponent],
+  declarations: [HeaderComponent, ButtonComponent, CardComponent, TagComponent, BarSearchComponent],
   imports: [CommonModule, HttpClientModule, AngularSvgIconModule.forRoot()],
-  exports: [HeaderComponent, ButtonComponent, CardComponent, TagComponent],
+  exports: [HeaderComponent, ButtonComponent, CardComponent, TagComponent, BarSearchComponent],
 })
 export class SharedModule {}
